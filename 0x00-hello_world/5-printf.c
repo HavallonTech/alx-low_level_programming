1fe8d20fe266d9308d10581cd94c979c979a83c2
kester geroge <kestercode@gmail.com>
2023-06-16 08:24:25 -0700

    C Programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Program written by George kester
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Program to print string using prinf  */
+char mystr1[105] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n",mystr1);
+return (0);
+}
