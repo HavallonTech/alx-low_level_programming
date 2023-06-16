4e9109f594c7835af1e95e7ad7b5c7929ff67d1d
kester geroge <kestercode@gmail.com>
2023-06-16 08:46:30 -0700

    C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+int main()
+{
+    int sizeint = sizeof(int);
+    int sizelong = sizeof(long);
+    //printf("Size of a char: %d byte(s)\n", sizeint);
+	printf("Size of an int: %d byte(s)\n", sizeint);
+	printf("Size of a long int: %d byte(s)\n", sizelong);
+	//printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	//printf("Size of a float: %d byte(s)\n", sizeof(float));
+}
