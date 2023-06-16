5873d9082cfa53bc12b3766f112ff04a432d7659
kester geroge <kestercode@gmail.com>
2023-06-16 15:17:28 -0700

    C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*Program written by George kester
+* Return: 0 whenever it is succesfull
+*/
 int main()
 {
-    int sizeint = sizeof(int);
-    int sizelong = sizeof(long);
-    //printf("Size of a char: %d byte(s)\n", sizeint);
-	printf("Size of an int: %d byte(s)\n", sizeint);
-	printf("Size of a long int: %d byte(s)\n", sizelong);
-	//printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	//printf("Size of a float: %d byte(s)\n", sizeof(float));
+int sizeint = sizeof(int);
+int sizelong = sizeof(long);
+int sizeshort = sizeof(short);
+int sizefloat = sizeof(float);
+int sizechar = sizeof(char);
+//int sizelong = sizeof(long);
+printf("Size of a char: %d byte(s)\n", sizechar);
+printf("Size of an int: %d byte(s)\n", sizeint);
+printf("Size of a long int: %d byte(s)\n", sizelong);
+printf("Size of a short: %d byte(s)\n", sizeshort);
+printf("Size of a float: %d byte(s)\n", sizefloat);
+return (0);
 }
