6c394c4cc5dcb2e1ba02a22373d543fa24ab4546
kester geroge <kestercode@gmail.com>
2023-06-16 07:58:25 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@ int main(void)
 {
 /* Program to print string using put */
 char mystr1[105] = "\"Programming is like building a multilingual puzzle";
-puts(mystr1);  
-return 0;
+puts(mystr1);
+return (0);
 }
