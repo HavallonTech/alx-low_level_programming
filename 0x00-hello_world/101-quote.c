b5c04aa12608a692633ed0cc6e7c8a0396486b81
Kester George <kestercode@gmail.com>
2023-06-16 16:00:27 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+* main - Entry point
+*Program written by George kester
+* Return: Always 0 when succefull
+*/
+int main(void)
+{
+char mystr1[105] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(2,mystr1,59)
+return (1);
+}
