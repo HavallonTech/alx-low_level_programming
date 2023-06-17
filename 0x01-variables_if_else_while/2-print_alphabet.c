53a9573c60cc87d1c0b3ad62296e081740620609
Kester George <kestercode@gmail.com>
2023-06-17 11:49:42 -0700

    Last digit of a randomly generated number

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
 /**
 * main - Entry point
+* Program to print lowercase of alphabet
 *Program written by George kester
-*Program to return last digit of a radomly
-*generated numbersand test the number for equality
 * Return: Return  0 whenever it is successfull
 */
 /* betty style doc for function main goes there */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there*/
-if ((n % 10) > 5)
-        printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-else if (((n % 10) < 6) && ((n % 10) != 0))
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-else
-        printf("Last digit of %d is %d and is 0\n", n, n % 10);
-return (0);
+char lowercase[26]="abcdefghijklmnopqrstuvwxyz";
+for(counter=0; counter < 26; counter++)
+{
+	putchar(lowercase[counter]);
 }
+putchar('\n');
+return (0);
+i}
