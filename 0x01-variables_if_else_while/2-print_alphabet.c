994958509c0377602c1cce86f2ba5aea343ee82a
Kester George <kestercode@gmail.com>
2023-06-17 11:55:51 -0700

    Last digit of a randomly generated number

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,7 @@ int main(void)
 {
 char lowercase[26] = "abcdefghijklmnopqrstuvwxyz";
 int counter;
-for(counter = 0; counter < 26; counter++)
+for (counter = 0; counter < 26; counter++)
 {
 	putchar(lowercase[counter]);
 }
