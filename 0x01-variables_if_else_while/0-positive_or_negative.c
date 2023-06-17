4c428e903ec31b0877e59647b212dbfd00062207
Kester George <kestercode@gmail.com>
2023-06-17 10:02:21 -0700

    C Programming if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,27 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- *Program written by George kester
- * Return: Return  0 whenever it is successfull
- */
- /* betty style doc for function main goes there */
+* main - Entry point
+*Program written by George kester
+* Return: Return  0 whenever it is successfull
+*/
+/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-	    printf("%d, is positive \n",n );
-	}else if (n==0)
-	{
-	    printf("%d, is zero \n",n);
-	}
-	else {
-	    printf("%d, is negative \n",n);
-	}
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d, is positive \n",n );
+}else if (n==0)
+{
+printf("%d, is zero \n",n);
+}
+else {
+printf("%d, is negative \n",n);
+}
+return (0);
 }
