bc53813eb1691d874252e60c8347d4dca3959754
Kester George <kestercode@gmail.com>
2023-06-17 12:49:12 -0700

    Print 1 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Program to print 1 to 9 using puutchar
+*Program written by George kester
+* Return: Return  0 whenever it is successfull
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int counter;
+for (counter = 48; counter < 58; counter++)
+	putchar(counter);
+putchar('\n');
+return (0);
+}
