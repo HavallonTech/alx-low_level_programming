de0205514b58f0cdf09cf74d9fa801f800fbf55d
Kester George <kestercode@gmail.com>
2023-06-19 15:21:16 -0700

    Low level programming with C

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Program to print all possible combination of single
+* digit numbers
+* program written George kester
+* Return: Return  0 whenever it is successfull
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int counter;
+for (counter = 48; counter < 58; counter++)
+{
+	for (counter=48; counter2 < 58; counter2++)
+	{
+		if (counter2>counter)
+		{
+
+			putchar(counter);
+			putchar(counter2);
+			if (counter != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
