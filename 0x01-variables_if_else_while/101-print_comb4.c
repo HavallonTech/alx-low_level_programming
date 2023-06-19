ca3ab686e8123b87c03d61d641bdb1bedc3952b3
Kester George <kestercode@gmail.com>
2023-06-19 15:57:42 -0700

    C Programming lood and decisions

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Program to print all possible combination of single
+* digit numbers
+* program written George kester
+* Return: Return  0 whenever it is successfull
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int counter;
+int counter1;
+int counter2;
+for (counter = 48; counter < 58; counter++)
+{
+	for (counter1 = 49; counter1 < 58; counter1++)
+	{
+	for (counter2 = 50; counter2 < 58; counter2++)
+	{
+		if (counter2 > counter1 && conter1 > counter)
+		{
+				putchar(counter);
+				putchar(counter1);
+				putchar(counter2);
+				if (counter != 55 || counter2 != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	}
+}
+putchar('\n');
+return (0);
+}
