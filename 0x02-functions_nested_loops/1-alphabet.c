4d8f24d8175ca9e2fff25717a62bd4f599840760
Kester George <kestercode@gmail.com>
2023-06-20 16:13:20 -0700

    Putchar printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print alphabet in lowercase
+ * C Programming form ALX classes
+ * Program written br George Kester
+ * Putchar task
+ * Return: 0
+ * The Main() is the ,ain body of the program
+ */
+int main(void)
+{
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar (alph);
+	}
+_putchar('\n');
+return (0);
+}
