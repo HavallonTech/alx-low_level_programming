e3f6d06fad0057f96476a495fa71d122a7178175
Kester George <kestercode@gmail.com>
2023-06-20 16:19:57 -0700

    Putchar printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,15 @@
  * Program written br George Kester
  * Putchar task
  * Return: 0
- * The Main() is the ,ain body of the program
+ * The Main() is the main body of the program
  */
 int main(void)
 {
-	char alph;
-	for (alph = 'a'; alph <= 'z'; alph++)
-	{
-		_putchar (alph);
-	}
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+	_putchar (alph);
+}
 _putchar('\n');
 return (0);
 }
