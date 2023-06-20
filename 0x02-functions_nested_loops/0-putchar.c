8df73a7f55b1059257570a7cffcdca49fdfbb77d
Kester George <kestercode@gmail.com>
2023-06-20 15:40:36 -0700

    Putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* C Programming form ALX classes
+ *  Program written br George Kester
+ * Putchar task
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
