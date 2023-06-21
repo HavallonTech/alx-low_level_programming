4e603d6d924bd18261f0d797655d0e1392c69a47
Kester George <kestercode@gmail.com>
2023-06-20 17:48:19 -0700

    Though loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- *
- * print alphabet x10 - Print 10 times  all alphabet in lowercase
- * Return: void.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by new line
+ * return: void
  */
 void print_alphabet_x10(void)
 {
 int count;
+
 for (count = 1; count < = 10; count++)
 {
 	char alph;
+
 	for (alph = 'a'; alph < = 'z'; alph++)
 	{
 		_putchar (alph);
