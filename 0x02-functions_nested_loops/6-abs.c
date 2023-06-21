234bbab5d3164249838486323bd1791fdae87024
Kester George <kestercode@gmail.com>
2023-06-21 06:53:52 -0700

    Updated nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - print absolute value of an integer
+ * Return: positve value of an int
+ * @n: is the char to be checked
+ */
+
+int _abs(int n)
+{
+if (n > 0)
+	return (n);
+else
+	return (-n);
+}
