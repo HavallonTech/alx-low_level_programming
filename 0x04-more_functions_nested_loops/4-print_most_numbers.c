b8c818dd0912b5937a3d8400a5a7538e7f8faf0f
Kester George <kestercode@gmail.com>
2023-06-22 15:41:30 -0700

    more on nested loop

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -funtion to  print 01356789 using _putchar only
+ * return: void since there is no value to return
+ */
+
+void print_most_numbers(void)
+{
+	char mychar;
+
+	mychar = '0';
+	while (mychar <= '9')
+	{
+		if (!(mychar == '4' || mychar == '2'))
+			putchar(mychar);
+		mychar++;
+	}
+	putchar('\n');
+}
