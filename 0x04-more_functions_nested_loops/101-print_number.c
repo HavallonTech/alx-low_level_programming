29de7899dc855dc80f5ddcf8f8663603203d5f8c
Kester George <kestercode@gmail.com>
2023-06-23 09:07:32 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer passed
+ * using only  _putchar function to print
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int testnum;
+
+	if (n < 0)
+	{
+		testnum = -n;
+		_putchar('-');
+	} 
+	else
+	{
+		testnum = n;
+	}
+
+	_putchar((n1 % 10) + '0');
+}
