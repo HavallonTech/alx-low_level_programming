dda805c5dbbfbe074b100cc31921c3e9be27d574
Kester George <kestercode@gmail.com>
2023-06-25 11:45:05 -0700

    ok

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *  main - fibonacci
+ *  Purpose - no hardcode
+ *  Return:  (void)
+ */
+int main(void)
+{
+	long double a;
+	long double b;
+	int i;
+	long double result;
+
+	a = 1;
+	b = 2;
+
+	printf("%.0Lf, %.0Lf, ", a, b);
+	for (i = 1; i <= 96; i++)
+	{
+		result = a + b;
+		a = b;
+		b = result;
+		printf("%.0Lf, ", result);
+	}
+	printf("\n");
+	return (0);
+}
