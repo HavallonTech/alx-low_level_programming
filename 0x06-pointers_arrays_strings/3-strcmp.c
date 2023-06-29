2546624405089856aa0d4b2f10c8a364dd9c59c0
Kester George <kestercode@gmail.com>
2023-06-29 09:07:39 -0700

    a function to compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - function to compare two strings
+ * @s1: first input string
+ * @s2: second inpute string
+ * Return: the difference between strings when both
+ * strings are not equal
+ * or return 0 when both are equal
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int strcount;
+
+	strcount = 0;
+	while (s2[strcount] != '\0' && s1[strcount] != '\0')
+	{
+		if (s2[strcount] != s1[strcount])
+			return (s1[strcount] - s2[strcount]);
+		strcount++;
+	}
+	return (0);
+}
