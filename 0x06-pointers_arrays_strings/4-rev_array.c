b076ad5f1c6ac4f59650c9025e79478e0ae4d229
Kester George <kestercode@gmail.com>
2023-06-29 09:55:32 -0700

    a function to reverse the content of an array of integer

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,10 @@ void reverse_array(int *a, int n)
 	int count;
 	int check;
 
-	for(count = 0; count < n--; count++ )
+	for(count = 0; count < n--; count++)
 	{
 		check = a[count];
 		a[count] = a[n];
 		a[n] = check;
-		//count++;
-
 	}
 }
