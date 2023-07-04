a4184d436950aa042a0c3a5bdaede5b4c7452c72
Kester George <kestercode@gmail.com>
2023-07-04 15:21:48 -0700

    More on strings and pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,20 +8,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int counter1;
-	unsigned int counter2;
+	int i;
+	int j;
 
-	counter1 = 0;
-	counter2 = 0;
-
-	while (counter1 < 8)
+	for (i = 0; i < 8; i++)
 	{
-		while (counter2 < 8)
-		{
-			_putchar(a[counter1][counter2]);
-			_putchar('\n');
-			counter2++;
-		}
-		counter1++;
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
 	}
 }
