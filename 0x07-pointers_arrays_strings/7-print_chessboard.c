b3f725d1149a252e02c4d745648986e7748c9f34
Kester George <kestercode@gmail.com>
2023-07-04 15:15:05 -0700

    More on strings and pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_chessboard - a function that prints the chessboard
+* @a: parameter array
+* Return: 0 is returned upon succeful completion
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int counter1;
+	unsigned int counter2;
+
+	counter1 = counter2 = 0;
+
+	while (counter1 < 8)
+	{
+		while (counter2 < 8)
+		{
+			_putchar(a[counter1][counter2]);
+			_putchar('\n');
+			counter2++
+		}
+		counter1++;
+	}
+}
