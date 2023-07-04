51d6cd6ae5a8c30f5b66d50d01470c55110aa63d
Kester George <kestercode@gmail.com>
2023-07-04 00:26:07 -0700

    More on pointer and arrays

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_memcpy - a function that copies memory area
+ *@dest: where memory  isto be  stored
+ *@src: memory to be copied
+ *@n: number of bytes found in the serach
+ *Return: Returns copied memory with number of bytes changed
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int counter;
+	int counter1;
+
+	counter = 0;
+	counter1 = 0;
+
+	for (; counter < counter1; counter++)
+	{
+		dest[counter] = src[counter];
+		n--;
+	}
+	return (dest);
+}
