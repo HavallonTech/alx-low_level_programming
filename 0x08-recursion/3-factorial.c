ffc99ebf9882beeab6102ab81f6b2f69e7278daf
Kester George <kestercode@gmail.com>
2023-07-08 11:24:29 -0700

    Function to return factorail without using a loop

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - a function to return the factorail of a given number
+ * @n:  input parameter
+ * Return: The factorail computation of n
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorail(n - 1));
+}
