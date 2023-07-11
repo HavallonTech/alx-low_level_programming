811b6d44ec1cfa65629af5db82614fac50e48f96
Kester George <kestercode@gmail.com>
2023-07-11 10:42:05 -0700

    Code corrections

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - writes the character c to stdout provided by ALX
+ * @c: The character to print to STDO
+ * Return: On success 1 is returned
+ * On error, -1 is returned, and errno is set appropriately when failed
  */
 int _putchar(char c)
 {
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/** prototype files comes here */
 int _putchar(char c);
 int _atoi(char *s);
-
+/**  main.h file */
 #endif
