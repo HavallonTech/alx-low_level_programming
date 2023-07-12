aafd35f18e99e33de5a4a1c6095d57c4d92af0a8
Kester George <kestercode@gmail.com>
2023-07-12 15:22:54 -0700

    Updated

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c:Ther character to print
- * Return: On success 1.
+ * _putchar - writes the character c to stdout provide by ALX
+ * @c:Ther character to print to stadard output
+ * Return: On success 1 and 0 if not
  * On error, -1 is returned and errno is set appropriately.
  */
 int _putchar(char c)
