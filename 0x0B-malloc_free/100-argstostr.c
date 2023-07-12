58a26b0a4801e763b80958cb0d7593786f7d9bc9
Kester George <kestercode@gmail.com>
2023-07-12 16:01:28 -0700

    Malloc updated

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,38 +6,26 @@
  * @av: double pointer array as argument two
  * Return: 0
  */
-
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int i;
-	int nth;
-	int r;
-	/**int k;*/
-	int count;
+	int i, nth, r, count;
 
-	count  = 0;
-	r = 0;
+	count  = r = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	while (i < ac)
+	for (i = 0; i < ac; i++)
 	{
 		for (nth = 0; av[i][nth]; nth++)
 			count++;
-		i++;
 	}
 	count += ac;
-
 	str = malloc(sizeof(char) * count + 1);
 
-	/*Failure to create the dynamic memory allocation */
 	if (str == NULL)
 		return (NULL);
-
-	/** Actuall concatination do here */
-	while (i < ac)
+	for  (i = 0; i < ac; i++)
 	{
 		for (nth = 0; av[i][nth]; nth++)
 		{
@@ -48,7 +36,6 @@ char *argstostr(int ac, char **av)
 		{
 			str[r++] = '\n';
 		}
-		i++;
 	}
 	return (str);
 }
