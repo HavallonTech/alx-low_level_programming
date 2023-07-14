5d4388164849cc3d2b77dc2dc045c1fe96374c25
Kester George <kestercode@gmail.com>
2023-07-14 15:16:21 -0700

    malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,7 @@
+/**
+ * main - Main program that calls other functions
+ * A C program to multiply two numbers
+ * @argc: number of arguments entered
+ * @argv: arguments vector passed
+ * Return: 0  once successful
+ */
