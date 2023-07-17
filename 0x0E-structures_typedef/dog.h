2f5cf57a9c2cedde0aacb56dfb9eaf71ff4291e6
Kester George <kestercode@gmail.com>
2023-07-17 15:22:59 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,31 +1,18 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * struct dog - Basic information of the dog
- * @name: First member of the dog struct
- * @age: Second member of the dog struct
- * @owner: Third member of the dog struct
- * Description: Decriptionof the dog
+ * init_dog - a program to initializes a variable of type struct dog
+ * @d: pointer to struct dog to be initialized
+ * @name: name to of the dog to initialize
+ * @age: age of dog to initialize
+ * @owner: owner of dog to initialize
  */
-struct dog
+void init_dog(struct dog *dg, char *name, float age, char *owner)
 {
-	float age;
-	char *owner;
-	char *name;
-
-};
-
-/**
- * dog_t - typedef for struct the dog
- * including  Prototype definition
- */
-
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-#endif
+	if (dg == NULL)
+		dg = malloc(sizeof(struct dog));
+	dg->name = name;
+	dg->age = age;
+	dg->owner = owner;
+}
