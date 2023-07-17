09a4185d15f3fee4af9942d4ed29fe458c5c0d83
Kester George <kestercode@gmail.com>
2023-07-17 15:02:03 -0700

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+0x0E. C - Structures, typedef
+With the following task
+Write a function that initialize a variable of type struct dog
+Write a function that prints a struct dog
+Definition of a new type dog_t as a new name for the type struct dog.
+Write a function that creates a new dog
+Write a function that frees dogs.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Basic information of the dog
+ * @name: First member of the dog struct
+ * @age: Second member of the dog struct
+ * @owner: Third member of the dog struct
+ * Description: Decriptionof the dog
+ */
+struct dog
+{
+	float age;
+	char *owner;
+	char *name;
+
+};
+
+/**
+ * dog_t - typedef for struct the dog
+ */
+
+typedef struct dog dog_t;
+/** Prototype definition */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+#endif
