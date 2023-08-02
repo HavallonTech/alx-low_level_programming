f7dd7081faf5fb465c27d8d8d88cccbe03525a75
HavallonTech <kestercode@gmail.com>
2023-08-02 08:54:17 -0700

    Updated

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list provided by ALX
+ * @n: integer datatype
+ * @next: points to the next node of our datastructure
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+#endif
