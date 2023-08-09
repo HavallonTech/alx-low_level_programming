204919cf9eea2decf2508e8a0bae074325165e40
HavallonTech <kestercode@gmail.com>
2023-08-09 09:53:46 -0700

    Recurssion update

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * wildcmp - A fuction to Compare stringsusing recurssion
+ * @s1: pointer to string parammeters
+ * @s2: pointer to string params
+ * Return: 0 when successful
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
