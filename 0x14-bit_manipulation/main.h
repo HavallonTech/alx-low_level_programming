489c393f90eb0f6f17139508638a3f3b37dfaafb
root <kestercode@gmail.com>
2023-08-31 12:52:38 -0700

    bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
