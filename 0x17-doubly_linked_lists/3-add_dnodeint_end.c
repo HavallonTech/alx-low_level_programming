f79558d3a57df9cbae0e54c4d1368a02432f5014
HavallonTech <kestercode@gmail.com>
2023-10-13 00:23:15 +0100

    addition at the tail end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end  - A function to to add node at the begining
+ * @head : Head parameter received
+ * @n : int n is the data to add
+ *
+ * Return: the added node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
+
+	dlistint_t *temp;
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+	newnode->n = n;
+	newnode->next = NULL;
+	if ((*head) == NULL)
+	{
+		newnode->prev = NULL;
+		(*head) = newnode;
+	}
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = newnode;
+	newnode->prev = temp;
+
+	return (newnode);
+}
