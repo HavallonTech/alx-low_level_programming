b83cceb98712ed3b5f045410380e3bdbec47698c
HavallonTech <kestercode@gmail.com>
2023-10-13 00:53:25 +0100

    ok

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,11 +8,16 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp;
+	dlistint_t *temp;	
+	
+	if (head == NULL)
+		return;
+	/*dlistint_t *temp;*/
 	if (head != NULL)
 	{
 		temp = head;
 		head = head->next;
 		free(temp);
 	}
+	free(head);
 }
