d0161cb139a87f7d90ca22d52cec5627d2ede4b5
HavallonTech <kestercode@gmail.com>
2023-10-12 19:07:52 +0100

    hmm

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_dlistint - A function to print elements of a doubly link list
+ * dlistint_len - A function to print elements of a doubly link list
  * @h : Head parameter received
  *
  * Return: Size_t signed integer data type
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t k;
 
