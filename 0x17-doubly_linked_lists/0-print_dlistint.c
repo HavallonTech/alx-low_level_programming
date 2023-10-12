b0a5523bb283a3789a9e42d2f7fc6cb38aa5ff77
HavallonTech <kestercode@gmail.com>
2023-10-12 23:05:13 +0100

    c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,14 +15,10 @@ size_t print_dlistint(const dlistint_t *h)
 	if (h == NULL)
 		return (k);
 
-	while (h->prev != NULL)
-	{
-		h = h->prev;
-	}
 	while (h != NULL)
 	{
 		k++;
-		printf(" %d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (k);
