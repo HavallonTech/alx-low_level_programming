2a4d33e9ee2eca07550133b12cd9413be1ee9af1
HavallonTech <kestercode@gmail.com>
2023-10-13 00:30:03 +0100

    c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -21,10 +21,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	newnode->n = n;
 	newnode->next = NULL;
-	if ((*head) == NULL)
+	if (*head == NULL)
 	{
 		newnode->prev = NULL;
 		(*head) = newnode;
+		return (newnode);
 	}
 
 	while (temp->next != NULL)
