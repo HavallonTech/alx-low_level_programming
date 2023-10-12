ec2ecf6ca85090a20f728c702dbe9b37098d9a6b
HavallonTech <kestercode@gmail.com>
2023-10-12 23:03:07 +0100

    c programming

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -7,14 +7,13 @@
 
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
-/*struct dlistint_s *head = NULL;*/
-/*struct dlistint_s *current = NULL; */
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-/* void insertatbegin(int data); */
-/*int printList(); */
+
+
 #endif
