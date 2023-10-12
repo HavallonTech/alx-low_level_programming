6be7a3d92f4f6272aa67589ea44c08a2a6e7e74f
HavallonTech <kestercode@gmail.com>
2023-10-13 00:47:49 +0100

    c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint  - A function to to add node at the begining
+ * @head : Head parameter received
+ * 
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	if (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
