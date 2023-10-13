2b9dd63bca60afae346b4f057ec2de7605b55c56
HavallonTech <kestercode@gmail.com>
2023-10-13 01:42:07 +0100

    c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - A function to to get node at index
+ * @head : Head parameter received
+ * @index : int indexn is the data to add
+ *
+ * Return: the added node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t check = 0;
+	dlistint_t *temp;
+
+	temp = head;
+
+	if (head == NULL)
+		return (NULL);
+	if (check == index)
+		return (head);
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		check++;
+		if (check == index)
+			return (temp);
+	}
+	return (temp);
+
+}
