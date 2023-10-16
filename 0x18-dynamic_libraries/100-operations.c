0ec4407137f6139a115add402ce4a6c54f2c4d8b
HavallonTech <kestercode@gmail.com>
2023-10-17 00:25:26 +0100

    shell

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * add - function that performs addition
+ * @a: numb1
+ * @b: numb2
+ * Return: the opration's result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function to performs
+ *@a: num1
+ *@b: num2
+ *Return: the operation result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *mul - function to performs
+ *@a: num1
+ *@b: num2
+ *Return: the operation result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *div - function to carry out division
+ *@a : num 1
+ *@b : num 2
+ *
+ *Return: double
+ */
+double div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *mod - function to carry out division
+ *@a : num 1
+ *@b : num 2
+ *
+ *Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
