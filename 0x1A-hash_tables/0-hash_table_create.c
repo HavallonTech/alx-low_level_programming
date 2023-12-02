9c6563d11ef9d9ef36231dc5390aba5aa54562cb
HavallonTech <kestercode@gmail.com>
2023-12-02 14:48:08 +0100

    Hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function that create a hash table
+ * @size: the size of the hash table
+ *
+ * Return: the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int counter;
+
+	hash_table_t *nwtable;
+
+	nwtable = malloc(sizeof(hash_table_t));
+
+	if (nwtable == NULL)
+		return (NULL);
+	nwtable->size = size;
+	nwtable->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (nwtable->array == NULL)
+	{
+		free(nwtable);
+		return (NULL);
+	}
+	while (counter < size)
+	{
+		/*initializing our newtable array*/
+		nwtable->array = NULL;
+		counter += 1;
+	}
+
+	return (nwtable);
+}
